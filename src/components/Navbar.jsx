543c1e62ae350694a4d8557f6293a17ee1a72b0f
Sudhir302 <cshakti302@gmail.com>
2025-06-01 18:57:08 +0530

    added the github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(){
             <a href="#tech-used" className="nav-section">TECH USED</a>
             <img src="3dweather.png" alt="logo" className="nav-logo"/>
             <a href="#futurework" className="nav-section">FUTURE WORK</a>
-            <a href="#source-code" className="nav-section">SOURCE CODE</a>
+            <a href="https://github.com/Sudhir302/Weatherly" className="nav-section">SOURCE CODE</a>
         </div>
     )
 }
